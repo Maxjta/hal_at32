857602dc06c33f57429e915b045e006ed6848f7c
jun tan <juntan@arterytek.com>
2025-09-18 10:18:59 +0800

    hal_at32: Modify hal_at32 drivers license.
    Modify hal_at32 dirvers license
    
    Signed-off-by: Jun tan <juntan@arterytek.com>

--- at32f402_405/cmsis/cm4/device_support/at32f402_405_conf_template.h
+++ at32f402_405/cmsis/cm4/device_support/at32f402_405_conf_template.h
@@ -4,14 +4,21 @@
   * @brief    at32f402_405 config header file
   **************************************************************************
   *
-  * Copyright (c) 2025, Artery Technology, All rights reserved.
+  * Copyright (c) 2025, Artery Technology.
   *
-  * The software Board Support Package (BSP) that is made available to
-  * download from Artery official website is the copyrighted work of Artery.
-  * Artery authorizes customers to use, copy, and distribute the BSP
-  * software and its related documentation for the purpose of design and
-  * development in conjunction with Artery microcontrollers. Use of the
-  * software is governed by this copyright notice and the following disclaimer.
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *
+  * 1. Redistributions of source code must retain the above copyright notice, this
+  * list of conditions and the following disclaimer.
+  *
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  * this list of conditions and the following disclaimer in the documentation and/or
+  * other materials provided with the distribution.
+  *
+  * 3. Neither the name of the copyright holder nor the names of its contributors
+  * may be used to endorse or promote products derived from this software without
+  * specific prior written permission.
   *
   * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
   * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
