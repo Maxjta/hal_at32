d9ec22e2212706f063c8e57c74a63afc44560648
jun tan <juntan@arterytek.com>
2025-09-23 10:03:54 +0800

    hal_at32: Modify hal_at32 drivers license to Apache 2.0.
    Modify hal_at32 dirvers license to Apache 2.0
    
    Signed-off-by: Jun tan <juntan@arterytek.com>

--- at32f423/drivers/src/at32f423_dac.c
+++ at32f423/drivers/src/at32f423_dac.c
@@ -6,19 +6,7 @@
   *
   * Copyright (c) 2025, Artery Technology.
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *
-  * 1. Redistributions of source code must retain the above copyright notice, this
-  * list of conditions and the following disclaimer.
-  *
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  * this list of conditions and the following disclaimer in the documentation and/or
-  * other materials provided with the distribution.
-  *
-  * 3. Neither the name of the copyright holder nor the names of its contributors
-  * may be used to endorse or promote products derived from this software without
-  * specific prior written permission.
+  * SPDX-License-Identifier: Apache-2.0
   *
   * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
   * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
@@ -29,7 +17,6 @@
   *
   **************************************************************************
   */
-
 #include "at32f423_conf.h"
 
 /** @addtogroup AT32F423_periph_driver
