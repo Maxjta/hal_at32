2aacc52518feec516944897b15934585cefd8fe9
jun tan <juntan@arterytek.com>
2025-09-28 10:30:13 +0800

    modify adc define

--- common_include/at32_adc.h
+++ common_include/at32_adc.h
@@ -11,9 +11,13 @@
 #if defined(CONFIG_SOC_SERIES_AT32F423)
 #include <at32f423_adc.h>
 #define AT32_ADC_COMM_PSC
+#define ADC12_ORDINARY_TRIG_SOFTWARE  0x1F
+#define AT32_ADC_5M
 #endif
 
 #if defined(CONFIG_SOC_SERIES_AT32F435_437)
 #include <at32f435_437_adc.h>
 #define AT32_ADC_COMM_PSC
+#define ADC12_ORDINARY_TRIG_SOFTWARE  0x1F
+#define AT32_ADC_5M
 #endif
